dee03bb23d251315858c9f3b429eb8c653e66cf8
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 11:51:34 -0700

    printing from z to a using putchar for loop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char io;
+for (io = 'z' ; io <= 'a' ; --io)
+putchar(io);
+putchar('\n');
+return (0);
+}
