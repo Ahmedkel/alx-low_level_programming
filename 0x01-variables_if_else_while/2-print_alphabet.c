fd561ba13904e0302f423e500368f3d6068f5a06
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 09:17:16 -0700

    printing the whole alphabet using just one puts

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * main - Entry point
- * Description: It generates a random number and take
- *		the last digit is greater than 5 or
- *		equal to 0 or less than 6 but not 0
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	int n, last_num;
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_num = n % 10;
-	if (last_num == 0)
+	int main(void)
+
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_num);
-	}
-	else if (last_num > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
-	}
-	return (0);
+		char i = 'a';
+
+		while (i <= 'z')
+		{
+			putchar(i);
+			i++;
+		}
+
+		putchar('\n');
+
+		return (0);
 }
