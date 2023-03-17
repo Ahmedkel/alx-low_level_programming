26fbc85528b430b2813f080bd5b9cde9d2565ff6
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 08:39:32 -0700

    printing the whole alphabet using just one puts

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
+int main() 
+	{
+	char c;
+    	for (c = 'a'; c <= 'z'; ++c)
+        putchar(c);
+    	return 0;
+	}
