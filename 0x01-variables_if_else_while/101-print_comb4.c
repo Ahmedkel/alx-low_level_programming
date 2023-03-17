1314ac3752e60e0f8a0a04c14f9d3ada3480bf08
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 14:30:50 -0700

    printing from all possible 3 numbers combinition with comma

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,28 +5,24 @@
  */
 int main(void)
 {
-	int num1, num2,num3;
-
-	for (num1 = 48; num1 <= 57; num1++)
-	{
-		for (num2 = 48; num2 <= 57; num2++)
-		{
-			for (num3 = 48; num3 <= 57; num3++)
-			{
-			if (num1 < num2)
-			{
-				putchar(num1);
-				putchar(num2);
-				putchar(num3);
-				if (num2 != 57 || num1 != 56 || num3 != 55)
-				{
-					putchar(44);
-					putchar(32);
-				}
-			}
-		}
-	}
+int n, p, m;
+for (n = 0 ; n <= 8 ; n++)
+{
+for (p = n + 1 ; p <= 9 ; p++)
+{
+for (m = m + 1 ; m <= 9 ; m++)
+{
+putchar('0' + n);
+putchar('0' + p);
+putchar('0' + m);
+if (i < 7)
+{
+putchar(',');
+putchar(' ');
 }
-	putchar('\n');
-	return (0);
+}
+}
+}
+putchar('\n');
+return (0);
 }
