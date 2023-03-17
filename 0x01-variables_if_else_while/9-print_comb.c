ae03510c770e44ba9963da5e7805430e4167cc68
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 12:13:30 -0700

    printing from all possible combinition sorted of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int io;
+for (io = 0 ; io <= 9 ; io++)
+{
+putchar(io + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
