862e9f53f82a28609ba63cc57f6f39c606e24d12
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 11:23:42 -0700

    printing from 0 to 9 using for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int io;
+for (io = 0 ; io <= 9 ; io++)
+printf("%d",io);
+putchar('\n');
+return (0);
+}
