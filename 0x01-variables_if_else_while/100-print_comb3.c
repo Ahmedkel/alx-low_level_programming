392bd5686705c469e14e0ed654f41ae09db7b786
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-17 13:09:03 -0700

    printing from all possible combinition with comma

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char nuk[3];
+for (int i = 0 ; i < 9 ; i++)
+{
+	for (int j = i + 1; j < 10; j++)
+	{
+		nuk[0] = i + '0';
+		nuk[1] = j + '0';
+		nuk[2] = '\n' ;
+		for (int k = 0; k < 3; k++)
+			{
+putchar(nuk);
+			}
+		}
+	}
+return (0);
+}
