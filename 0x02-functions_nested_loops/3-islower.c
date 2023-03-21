4cad15409797edb89ae245a4ea6cec4fd2edc33a
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 09:31:35 -0700

    check if alphabet is lower return 1 if lower and 0 if upper using if using _putchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code .
+ * @c : parameter
+ * Return: 1 if c is lower .
+ * Return: 0 if c is upper .
+ */
+int _islower(int c)
+{
+ if ( c >= 'a' && c <= 'z')
+ {
+	 return(1);
+ }
+ else
+ {
+	 return(0);
+	}
+	printf('\n');
+}
