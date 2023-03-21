2bacd48fa9cd264dd41dd498d6c4eff2fa080ab5
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 08:26:24 -0700

    print alphabet without ascii and using _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@
 void print_alphabet(void)
 {
 	char chr;
-        for (chr = 'a'; chr <= 'z'; chr++)
-	_putchar(chr);
-	_putchar('\n');
+	for (chr = 'a'; chr <= 'z'; chr++)
+		_putchar(chr);
+		_putchar('\n');
 }
