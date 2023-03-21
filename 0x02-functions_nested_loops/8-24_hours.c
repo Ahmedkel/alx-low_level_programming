dbd842a3ce2dd47f8988f5b7d8cee428f13de538
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 13:29:19 -0700

    printing clock using /10 %48 return using _putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int hours ;
 	int min ;
-	for (hours = 0 ; hours <= 23 ; hours++)
+	for (hours = 0 ; hours < 24 ; hours++)
 	{
-		for (hours = 0 ; min <= 59 ; min++)
+		for (hours = 0 ; min < 60 ; min++)
 		{
 			_putchar((hours / 10) + 48);
 			_putchar((hours % 10) + 48);
