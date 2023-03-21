c4aa273d6c17504ba3cde10b7437da310664c4d6
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 16:50:49 -0700

    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89. By considering the terms in the Fibonacci sequence whose values do not exceed 4,000,000, write a program that finds and prints the sum of the even-valued terms, followed by a new line.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+unsigned long int j, k, next, sum;
+j = 1;
+k = 2;
+sum = 0;
+for (i = 1; i <= 33; ++i)
+{
+if (j < 4000000 && (j % 2) == 0)
+{
+sum = sum + j;
+}
+next = j + k;
+j = k;
+k = next;
+}
+printf("%lu\n", sum);
+return (0);
+}
