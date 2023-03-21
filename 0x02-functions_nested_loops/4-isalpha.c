38c5cd21c330f9620dea9fe19e6406280228f66d
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 09:53:14 -0700

    check if alphabet is lower return 1 if lower or upper and 0 if not alphabet using if using _putchar

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: character to check
+ * Return: 1 if lower or uppercase, 0 if not alphabet character
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z'|| c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
