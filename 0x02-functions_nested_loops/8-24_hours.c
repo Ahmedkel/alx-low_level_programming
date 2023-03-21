6c7250f35e7183983b84b683a392c370bfc709d6
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-21 13:09:20 -0700

    printing clock using /10 %48 return using _putchar

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * jack_bauer - print every minute of a day
+ * Description: print minutes 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+	while (hours <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hours++;
+	}
