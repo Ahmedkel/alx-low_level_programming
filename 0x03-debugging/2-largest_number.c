b6387bff709ab23a33db56903f829784fb6c77db
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-22 09:31:16 -0700

    a>b a>c and b>a b>c else it c that is bigger

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a >= b && a >= c)
-    {
-	    largest = a;
-    }
+	{
+		largest = a;
+	}
 	else if (b >= a && b >= c)
-    {
-	    largest = b;
-    }
+	}
+	largest = b;
+	}
 	else
-    {
-	    largest = c;
-    }
+	}
+		largest = c;
+		}
 
 	return (largest);
 }
