221123c2a663e7fe4ca20d0b9308f10dcc4fc89f
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-23 11:03:00 -0700

    printing a fizz and buzz 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,20 +6,29 @@
   * Return: Always 0 (Success)
   */
 
-int main() {
-    int fizzbuzz;
-    for (fizzbuzz = 0; fizzbuzz < 100; fizzbuzz++) {
-        if (fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0) {
-            printf("fizzbuzz\n");
-            continue;
-        } else if (fizzbuzz % 3 == 0) {
-            printf("fizz\n");
-            continue;
-        } else if (fizzbuzz % 5 == 0) {
-            printf("buzz\n");
-            continue;
-        }
-        printf("%d\n", fizzbuzz);
-    }
-    return 0;
+int main(void)
+{
+	int fizzbuzz;
+
+	for (fizzbuzz = 0; fizzbuzz < 100; fizzbuzz++)
+	{
+		if (fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0)
+		{
+			printf("fizzbuzz\n");
+			continue;
+		}
+		else if (fizzbuzz % 3 == 0)
+		{
+			printf("fizz\n");
+			continue;
+		}
+		else if (fizzbuzz % 5 == 0)
+		{
+			printf("buzz\n");
+			continue;
+		}
+		printf("%d\n", fizzbuzz);
+	}
+	return (0);
 }
+
