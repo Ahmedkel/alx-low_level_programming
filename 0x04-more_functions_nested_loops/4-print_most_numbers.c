d67cd19ee63fb1f7059851c0bc47dbe10474a2cb
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-23 07:57:51 -0700

    printing from 0 to 9 but without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_most_numbers - print 0 to 9, skip 2 and 4
@@ -9,11 +9,9 @@ void print_most_numbers(void)
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
 	if (i != '2' && i != '4')
 	{
 		_putchar(i);
-	}
 	}
 	_putchar('\n');
 }
