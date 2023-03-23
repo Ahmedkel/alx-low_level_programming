45fc7e5ceb59112ba60adc5f5576dd91f0cf03bb
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-23 06:46:16 -0700

    check if the letter is upper return 1 else return 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isupper - checks for lowercase character
  * @c: character to check
  * Return: 1 if lowercase, 0 if uppercase
  */
