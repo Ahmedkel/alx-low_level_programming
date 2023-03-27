df4ca59143355bbed95b65e850b4e30f022e7bd2
Ahmedkel <ahmedkeloch@gmail.com>
2023-03-27 15:17:19 -0700

    Write a function that decode some weird stuff and it was insane

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 84
+#define MIN_CHAR 33
+#define MAX_CHAR 126
+#define PASSWORD_SUM 2772
+
+/**
+ * main - function that generates a password and prints it to the console.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i, sum = 0, diff, a, b;
+
+    srand(time(NULL));
+
+    while (sum < PASSWORD_SUM)
+    {
+        password[i] = MIN_CHAR + rand() % (MAX_CHAR - MIN_CHAR + 1);
+        sum += password[i];
+        i++;
+    }
+    password[i] = '\0';
+
+    diff = sum - PASSWORD_SUM;
+    a = diff / 2;
+    b = diff - a;
+
+    for (i = 0; password[i]; i++)
+    {
+        if (password[i] >= MIN_CHAR + a)
+        {
+            password[i] -= a;
+            break;
+        }
+    }
+    for (i = 0; password[i]; i++)
+    {
+        if (password[i] >= MIN_CHAR + b)
+        {
+            password[i] -= b;
+            break;
+        }
+    }
+
+    printf("%s", password);
+    return 0;
+}
