aa7df034b8ac2e183e3eda9e91f62a48603afdaf
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-05 10:05:51 -0700

    using recusion to return factorial of giving number but -1 if negative 1 if equal to 0 and factorial of n otherwise

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* factorial - computes the factorial of a given number.
+*
+* @n: input number.
+*
+* Return: the factorial of n or -1 if number is negative..
+*/
+int factorial(int n)
+{
+	return (n < 0 ? -1 : n == 0 : ? 1 : n * factorial(n-1));
+}
