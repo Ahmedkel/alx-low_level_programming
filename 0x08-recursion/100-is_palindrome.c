8ce9b96f74488c811497d299b291cbbcfdb80630
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-05 18:19:30 -0700

    alredy hard stuff??

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,9 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-    if (*s == 0)
-        return (1);
-    return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -24,9 +24,9 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -39,9 +39,9 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-    if (*(s + i) != *(s + len - 1))
-        return (0);
-    if (i >= len)
-        return (1);
-    return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
