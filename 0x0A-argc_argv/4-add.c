6269874b928b4e74f1c6ca4a1368fda6f615a364
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-06 16:38:19 -0700

    Write a program that adds positive numbers return error for char and sum for normal num

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - Entry Point.
+ *
+ * Description: adds positive numbers.
+ *
+ * @argc: number of args
+ * @argv: an array of args
+ *
+ * Return: 0 if success otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, arg, j;
+
+	for (arg = 1; arg < argc; arg++)
+	{
+		for (j = 0; argv[arg][j] != '\0'; j++)
+			if (!isdigit(argv[arg][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[arg]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
