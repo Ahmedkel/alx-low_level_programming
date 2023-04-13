43a897989d0dd8d65bdd2717a3e358ba8417c868
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-13 15:06:51 +0000

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,37 +1,43 @@
-#include "main.h"
+#include <stdlib.h>
 
-/** 
- * string_nconcat - Concatenates two strings 
- * @s1: First string 
- * @s2: Second string 
- * @n: Number of characters from s2 to use 
- * Return: Pointer to allocated memory containing the concatenated string 
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of characters from s2 to use
+ *
+ * Return: Pointer to allocated memory containing the concatenated string
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{ 
-
-	size_t len1 = 0;
-	size_t len2 = 0;
-	size_t len = 0;
+char *string_nconcat(char *s1, char *s2, size_t n)
+{
+	size_t len1 = 0, len2 = 0, len = 0;
+	char *concat = NULL;
 
 	if (s1 != NULL)
 		len1 = strlen(s1);
+
 	if (s2 != NULL)
 		len2 = strlen(s2);
 
+	if (n >= len2)
+		n = len2;
+
 	len = len1 + n + 1;
-  
-	char *p = malloc(len * sizeof(char));
-    
-	if (p == NULL)
-		return NULL;
 
-	strcpy(p, s1);
+	concat = malloc(len * sizeof(char));
 
-	strncpy(p + len1, s2, n);
+	if (concat == NULL)
+		return (NULL);
 
-	p[len - 1] = '\0';
+	if (s1 != NULL)
+		strcpy(concat, s1);
+	else
+		concat[0] = '\0';
 
-	return (p); 
+	strncat(concat + len1, s2, n);
+
+	concat[len - 1] = '\0';
+
+	return (concat);
 }
