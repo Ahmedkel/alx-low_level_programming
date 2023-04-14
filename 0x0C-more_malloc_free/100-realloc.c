e8d76a3cd90a836d8810a91949b4b1d8d03b608a
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-14 02:44:06 +0000

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,48 +1,31 @@
-#include "main.h" 
-  
- /** 
-  * _realloc - reallocates a memory block using malloc and free 
-  * @ptr: pointer to the memory previously allocated 
-  * @old_size: the size, in bytes, of the allocated space for ptr 
-  * @new_size: the new size, in bytes of the new memory block 
-  * Return: pointer is worked, NULL otherwise. 
-  */ 
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) 
- { 
-         char *pointer; 
-         unsigned int i; 
-  
-         if (new_size == old_size) 
-                 return (ptr); 
-  
-         if (ptr == NULL) 
-         { 
-                 pointer = malloc(new_size); 
-  
-                 if (pointer == NULL) 
-                         return (NULL); 
-  
-                 return (pointer); 
-         } 
-         else 
-         { 
-                 if (new_size == 0) 
-                 { 
-                         free(ptr); 
-                         return (NULL); 
-                 } 
-         } 
-  
-         pointer = malloc(new_size); 
-  
-         if (pointer == NULL) 
-                 return (NULL); 
-  
-         for (i = 0; i < old_size && i < new_size; i++) 
-         { 
-                 pointer[i] = ((char *) ptr)[i]; 
-         } 
-  
-         free(ptr); 
-         return (pointer); 
- }
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ *
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
+	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
+}
