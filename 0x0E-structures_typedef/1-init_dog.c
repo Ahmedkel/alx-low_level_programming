e680ec6f1c67e7a4429d1308f615ab3719e6a34a
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-17 13:51:05 +0000

    Define a new type struct dog with the following elements

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
-
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
