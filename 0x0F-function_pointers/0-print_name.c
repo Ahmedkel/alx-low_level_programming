feb7955f60ef8a603512f56f4af88c0fa1039e93
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-19 13:59:12 +0000

    Write a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing(0)
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || f)
+	{
+	f(name);
+	}
+}
