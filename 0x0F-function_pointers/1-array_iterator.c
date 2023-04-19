ac69a7a1a4082439f8ad42925b2118bfeecaa22c
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-19 15:40:25 +0000

    Write a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,11 +17,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	
-	size_t i = 0;
+	i = 0;
 
 	while (i < size)
-    	{
-	action(array[i]);
-	i++;
+	{
+		action(array[i]);
+		i++;
 	}
 }
