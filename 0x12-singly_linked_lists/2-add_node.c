8529621309bd7915109e1c3e6c0e1581134b7909
Ahmedkel <ahmedkeloch@gmail.com>
2023-04-27 18:22:29 +0000

    Write a function that adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to pointer to the head of the list
+ * @str: string to be added to the list
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = _strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: length of the string
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len]; len++)
+		;
+	return (len);
+}
