9ab275b456fb48b30a18ffc7e613fc2096ac247a
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-19 11:36:34 +0100

    fixing and cleaning up the code

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,15 +1,1 @@
-#include "main.h"
 
-/**
- * _isdigit - check the code
- *@c :parameter
- * Return: Always 0.
- */
-
-int _isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
-}
--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,11 +1,1 @@
-#include "main.h"
 
-/**
-     * _isalpha - checks for alphabetic character
-     * @c: the character to be checked
-     * Return: 1 if c is a letter, 0 otherwise
-     */
-int _isalpha(int c)
-{
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
-}
