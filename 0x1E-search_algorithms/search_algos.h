1d2365d09f0faae9a29986ece78aab959b04db3a
Ahmedkel <ahmedkeloch@gmail.com>
2023-10-10 15:33:25 +0100

    setting up the file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGO_
+#define _SEARCH_ALGO_
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+
+#endif
